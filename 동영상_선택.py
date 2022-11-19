6791f6812f9268b640ab59faf870511387ac7672
kanu01 <103395394+kanu01@users.noreply.github.com>
2022-11-19 11:51:55 +0900

    Update 동영상_선택.py

--- 동영상_선택.py
+++ 동영상_선택.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+
 # 사용할 색 BGR 정의
 red=(0,0,255)
 green=(0,200,0)
@@ -16,13 +17,15 @@ image_mask2=cv2.resize(image_mask2,(400,280))
 
 image_mask1_h, image_mask1_w, _ = image_mask1.shape
 image_mask2_h, image_mask2_w, _ = image_mask2.shape
-## 클릭할 마스크 아이콘 정의
+
+## 클릭할 마스크 아이콘 정의 (아이콘 클릭시 마스크 씌워줌)
 image_icon1 = cv2.imread('python\\opencv\\project\\icons\\pink_icon.png')
 image_icon2 = cv2.imread('python\\opencv\\project\\icons\\yellow_icon.png')
 image_icon1 = cv2.resize(image_icon1, (120, 100))
 image_icon2 = cv2.resize(image_icon2, (120, 100))
 mp_face_detection = mp.solutions.face_detection   ## 얼굴 검출을 위한 face_detection 모듈 사용
 mp_drawing = mp.solutions.drawing_utils  ## 얼굴의 특징을 그리기 위한 drawing_utils 모듈 사용
+
 ## 동영상 파일 열기
 cap = cv2.VideoCapture('python\\opencv\\project\\twokids.mp4')
 a = False
